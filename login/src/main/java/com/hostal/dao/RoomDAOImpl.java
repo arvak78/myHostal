a9c6d596b7311e22d19010230fdb1739225f8e11
manel <arvak7@hotmail.com>
2016-02-11 21:15:09 +0100

    add bootstrap

--- login/src/main/java/com/hostal/dao/RoomDAOImpl.java
+++ login/src/main/java/com/hostal/dao/RoomDAOImpl.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.inject.Named;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
