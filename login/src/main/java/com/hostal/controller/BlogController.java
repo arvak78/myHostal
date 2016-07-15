012a16f06f5e8520ec761762287bd1c79feb763b
manel <arvak7@hotmail.com>
2016-07-15 17:29:03 +0200

    blog

--- login/src/main/java/com/hostal/controller/BlogController.java
+++ login/src/main/java/com/hostal/controller/BlogController.java
@@ -5,20 +5,21 @@ import com.hostal.persistence.Posts;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.PostConstruct;
-import javax.inject.Named;
 import java.util.List;
 
 /**
  * Created by Manel on 14/04/2016.
  */
 @Controller
-//@RequestMapping("/blog")
+@RequestMapping("/blog")
 //@Named
 public class BlogController {
 
@@ -36,13 +37,24 @@ public class BlogController {
         return allPosts;
     }
 
+    @RequestMapping("/posts")
+    public ModelAndView showPosts() {
+
+        return new ModelAndView("blog", "misposts", allPosts);
+    }
+
     public void setAllPosts(List<Posts> allPosts) {
         this.allPosts = allPosts;
     }
 
+    @ModelAttribute("timeline")
+    public List<Posts> populateTimeline() {
+        return allPosts;
+    }
+
     @RequestMapping(value="/paint", method = RequestMethod.POST)
-    public ModelAndView paintSelectedPost(ModelMap model,
-                                  @ModelAttribute("post") Posts post) {
+    public ModelAndView paintSelectedPost(@ModelAttribute("post") Posts post, BindingResult result, ModelMap model) {
+/* public ModelAndView paintSelectedPost(@RequestParam("user") String user) { */
 
 
         System.out.println();
@@ -50,7 +62,7 @@ public class BlogController {
         String message = "aaa";
 
 
-        return new ModelAndView("starter", "message", message);
+        return new ModelAndView("blog", "torna", message);
 
     }
 
